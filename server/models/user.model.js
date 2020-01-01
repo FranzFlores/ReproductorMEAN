94b5d6f0edc996ebd769e4334121c6ccb9acd28f
Franz Flores <andresfloresgallardo@gmail.com>
2020-01-01 18:39:13 -0500

    Modelo User
    Modelo de la entidad usuario en la BD

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,1 +1,15 @@
 'use strict'
+//Esquema para los usuarios en la BBDD
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+var userSchema = new Schema({
+    name: { type: String, required: true },
+    userName: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    role: { type: String, required: true },
+    image: { type: String, required: true }
+},{timestamps: {createdAt: 'created_at', updatedAt: 'update_at'}});
+
+module.exports = mongoose.model("User",userSchema);
